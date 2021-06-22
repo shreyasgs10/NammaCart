b56a09866e36ea9332b1652b2574dc179f84b592
shreyasgs10 <shreyasgs10@gmail.com>
2021-06-22 14:00:46 +0530

    first commit

---
+++ js/firebase-init.js
@@ -0,0 +1,12 @@
+// Initialize Firebase
+var firebaseConfig = {
+    apiKey: "AIzaSyBlG_FfXlffLi9ivpjEpC6DQCCdDYtrNkQ",
+    authDomain: "nammacart-658cd.firebaseapp.com",
+    databaseURL: "https://nammacart-658cd-default-rtdb.firebaseio.com",
+    projectId: "nammacart-658cd",
+    storageBucket: "nammacart-658cd.appspot.com",
+    messagingSenderId: "508159254659",
+    appId: "1:508159254659:web:93cb876ac5965c4c467bb6"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
